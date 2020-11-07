bb1c6359ff19ac0d64ce0f84fa69a12fc8d07038
StJivko <jivko1913@gmail.com>
2020-11-07 18:50:31 +0100

    Nested Loop Lab

---
+++ PBJava/src/NestedLoopLab/MultiplicationTable.java
@@ -0,0 +1,15 @@
+package NestedLoopLab;
+
+import java.util.Scanner;
+
+public class MultiplicationTable {
+    public static void main(String[] args) {
+
+        for (int i = 1; i <= 10; i++) {
+            for (int x = 1; x <= 10; x++) {
+                int product = i * x;
+                System.out.printf("%d * %d = %d%n",i ,x ,product);
+            }
+        }
+    }
+}
